73ed985741b1c73588631af7ee7001d5f867c96d
taakuuyaa <51745522+taakuuyaa@users.noreply.github.com>
2021-10-27 00:03:51 +0900

    ⬆️ update test

--- test/my_first_gem_test.rb
+++ test/my_first_gem_test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require "./test/test_helper"
 
 class MyFirstGemTest < Minitest::Test
   def test_that_it_has_a_version_number
@@ -8,6 +8,10 @@ class MyFirstGemTest < Minitest::Test
   end
 
   def test_it_does_something_useful
-    assert false
+    assert true
+  end
+
+  def test_greet
+    assert_output("Hello, taakuuyaa! Your first gem\n") { MyFirstGem.greet('taakuuyaa') }
   end
 end
